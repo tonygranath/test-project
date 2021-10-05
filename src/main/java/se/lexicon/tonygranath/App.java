f8ac4a307163b0da80a2ba343ca59f051b4556a3
Tony Granath <tony.granath@gmail.com>
2021-10-05 11:44:32 +0200

    test

---
+++ src/main/java/se/lexicon/tonygranath/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.tonygranath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
